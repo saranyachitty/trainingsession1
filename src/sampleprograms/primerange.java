5eb8bc45f35a30313f1cc5c95095ac76ce310d45
saranyachitty <104919231+saranyachitty@users.noreply.github.com>
2022-05-05 16:34:10 +0530

    prime2

---
+++ src/sampleprograms/primerange.java
@@ -0,0 +1,29 @@
+package sampleprograms;
+
+import java.util.Scanner;
+
+public class primerange
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Enter max number");
+		Scanner in=new Scanner(System.in);
+		int maxnumber=in.nextInt();
+		
+		for(int num=2;num<=maxnumber;num++)
+		{
+			boolean prime=true;
+			for(int i=2;i<=num/2;i++)
+			{
+				if(i%2==0)
+				{
+					prime=false;
+					break;
+				}
+			}
+			if(prime==true)
+				System.out.println(num);
+		}
+	}
+}
